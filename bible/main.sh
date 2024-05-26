be5602069ec7c214e9be6d4f86a0751babe9a025
Arthur Henrique <arthur393@gmail.com>
2024-05-26 16:02:18 -0300

    go

--- bible/main.sh
+++ bible/main.sh
@@ -10,7 +10,21 @@ mkdir -p bible/$BIBLE_BOOK
 div=${bible_books[$BIBLE_BOOK]}
 
 seq $(($(date "+%d") * $div - $div)) $(($(date "+%d") * $div)) | \
-xargs -I@ curl -Ss https://www.abibliadigital.com.br/api/verses/acf/$BIBLE_BOOK/@ | \
-jq '.verses[].text' > bible/$BIBLE_BOOK/day_$(date "+%d")
+    xargs -I@ curl -Ss https://www.abibliadigital.com.br/api/verses/acf/$BIBLE_BOOK/@ | \
+    jq '.verses[].text' | \
+    sed 's/\"//g' > tmp
+
+while IFS= read -r line; do
+  echo "$line"
+
+  ((line_count++))
+
+  if (( line_count == 3 )); then
+    echo ""
+    line_count=0
+  fi
+done < "tmp" > "bible/$BIBLE_BOOK/day_$(date "+%d")"
+
+rm -rf tmp
 
 echo "- [$BIBLE_BOOK day_$(date "+%d")](bible/$BIBLE_BOOK/day_$(date "+%d"))" >> README.md
