e03610177ea0a52c25e85e4830a6d6681177809a
Arthur Henrique <arthurhenrique@users.noreply.github.com>
2024-05-26 15:49:40 -0300

    go

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Read psalms
 
-- [bible/sl/day_26](bible/sl/day_26)
-- [sl day_26](bible/sl/day_26)
---
+++ bible/main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+declare -A bible_books
+bible_books["sl"]="5" # 130 cap / 30 day of month
+
+BIBLE_BOOK="sl"
+
+mkdir -p bible/$BIBLE_BOOK
+
+div=${bible_books[$BIBLE_BOOK]
+
+seq $(($(date "+%d") * $div - $div)) $(($(date "+%d") * $div)) | \
+xargs -I@ curl -Ss https://www.abibliadigital.com.br/api/verses/acf/$BIBLE_BOOK/@ | \
+jq '.verses[].text' > bible/$BIBLE_BOOK/day_$(date "+%d")
+
+echo "- [$BIBLE_BOOK day_$(date "+%d")](bible/$BIBLE_BOOK/day_$(date "+%d"))" >> README.md
